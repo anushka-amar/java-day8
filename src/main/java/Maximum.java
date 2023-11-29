4bf241066741d39c94361af8f0cbdb41ee581712
Anushka <anushkamar1102@gmail.com>
2023-11-29 21:59:26 +0530

    Refactored UC4: extended the max method to take variable number of arguments

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,27 +1,22 @@
+import java.util.*;
+
 public class Maximum <T extends Comparable<T>>{
 
-    private T x, y, z;
+    private List<T> list;
 
-    public Maximum(T x, T y, T z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public Maximum(T... list){
+        this.list = Arrays.asList(list);
     }
-
     public T testMaximum (){
-        T res = Maximum.find_max(x,y,z);
+        T res = find_max();
         return res;
     }
 
-    public static <T extends Comparable<T>> T find_max(T x, T y, T z){
-        T max = x;
-        if(y.compareTo(max)>0){
-            max = y;
-        }
-        if(z.compareTo(y)>0){
-            max = z;
-        }
-        return max;
+    public T find_max(){
+        Collections.sort(list);
+        int index = list.size();
+        return list.get(index-1);
     }
+
 
 }
