90999b9c5f157998bb5fdcfbde243494e988c046
Anushka <anushkamar1102@gmail.com>
2023-11-29 14:46:12 +0530

    test: TC-2.3 finding max when max_float_value at 3rd position

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -23,7 +23,7 @@ public class Maximum {
     public static void main(String[] args) {
         Float a = 50.20f;
         Float b = 50.35f;
-        Float c = 50.06f;
+        Float c = 50.75f;
 
         System.out.println("The maximum out of the three float type is "+find_max(a,b,c));
     }
