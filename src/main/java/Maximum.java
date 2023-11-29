e68a8022ea6f669cab8d1b70eba99e5fcb28d3ba
Anushka <anushkamar1102@gmail.com>
2023-11-29 19:47:04 +0530

    Refactor1: creating a generic method for all types and testing the test-cases

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,40 +1,22 @@
 public class Maximum {
-    public static Integer find_max(Integer x, Integer y, Integer z){
-        Integer max = x;
+
+    public static <T extends Comparable<T>> T find_max(T x, T y, T z){
+        T max = x;
         if(y.compareTo(max)>0){
             max = y;
         }
-        if(z.compareTo(max)>0){
+        if(z.compareTo(y)>0){
             max = z;
         }
         return max;
     }
-    public static Float find_max(Float x, Float y, Float z){
-        Float max = x;
-        if(y.compareTo(max)>0){
-            max = y;
-        }
-        if(z.compareTo(max)>0){
-            max = z;
-        }
-        return max;
-    }
-    public static String find_max(String x, String y, String z){
-        String max = x;
-        if(y.compareTo(max)>0){
-            max = y;
-        }
-        if(z.compareTo(max)>0){
-            max = z;
-        }
-        return max;
-    }
+
+
 
     public static void main(String[] args) {
-        String x = "Orange";
-        String y = "Peach";
-        String z = "pineapple";
 
-        System.out.println("The max out of the three strings lexicographically is "+find_max(x,y,z));
+        System.out.println("The max out of the three integer type number is "+find_max(75, 50, 55));
+        System.out.println("The max out of the three float type number is "+find_max(50.45f, 50.75f, 50.5f));
+        System.out.println("The max out of the three strings lexicographically is "+find_max("Orange", "Peach", "PineApple"));
     }
 }
