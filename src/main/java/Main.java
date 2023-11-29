6ae02331d9511d91472ef62d83177dfb475dffdf
Anushka <anushkamar1102@gmail.com>
2023-11-29 20:12:22 +0530

    Refactor2: TC-3.2.1 creating object of Integer type and checking maximum

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Test-case1
+        Maximum <Integer> obj1 = new Maximum<>(75,60,45);
+        System.out.println("The max Integer type of the three is "+obj1.testMaximum());
+    }
+}
--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -23,5 +23,5 @@ public class Maximum <T extends Comparable<T>>{
         }
         return max;
     }
-    
+
 }
